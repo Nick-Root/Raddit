7d6cc6476427b30bd1a2a67000853bee0bc87d87
nick <nickroot168@gmail.com>
2024-01-10 16:28:13 -0500

    seeds render fix

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -19,9 +19,9 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
-        db.session.execute(f"TRUNCATE table {SCHEMA}.communities RESTART IDENTITY CASCADE;")
         db.session.execute(f"TRUNCATE table {SCHEMA}.posts RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.communities RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
         # undo_users()
         # undo_communities()
         # undo_posts()
